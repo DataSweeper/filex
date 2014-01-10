f1512f602252b1726bf7249ae7bb6a8bb9f6087a
SGnan <sivagnanam.student@gmail.com>
2014-01-10 09:34:52 +0530

    Update filex.rb

--- filex.rb
+++ filex.rb
@@ -1,12 +1,43 @@
-#ruby program to simplify the file exange between two mechines
+#ruby program to simplify the file exange between two mechine
 
-file = ARGV[0]
-name = ARGV[1]
-
-if name == "devop"
-  destination = "devop@192.168.24.249:/home/devop/"
+while true
+  puts "1 Register"
+  puts "2 File Transfer"
+  puts "3 Quit"
+  option = gets.chomp
+  case option
+    when "1"
+      puts "Enter user name"
+      uname = gets.chomp
+      puts "Enter machine name"
+      machine = gets.chomp
+      puts "Enter IP"
+      ip = gets.chomp
+      aFile = File.new("Register.txt", "a+")
+      aFile.puts uname+":"+machine+":"+ip
+      aFile.close
+    when "2"
+      puts "Enter user name"
+      uname = gets.chomp
+      puts "Enter machine name"
+      machine = gets.chomp
+      puts "Enter file"
+      file = gets.chomp
+      IO.foreach("Register.txt") { |line|
+        split_array = line.chomp.split(":")
+        if uname == split_array[0]
+          if machine == split_array[1]
+            puts split_array[0], split_array[1]
+            destination = split_array[0]+'@'+split_array[2]+':/home/'+split_array[0]+'/'
+            puts destination
+            cmd = "scp #{file} #{destination}"
+            exec( cmd )
+	  end
+        end
+	}
+    when "3"
+      exit
+    else
+       puts "Enter valid option"
+  end
 end
-cmd = "scp #{file} #{destination}"
-exec( cmd )
-puts "file transmited" 
-
